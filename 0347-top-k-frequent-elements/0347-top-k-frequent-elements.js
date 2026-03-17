f56090d24848b80c860231d195fe197481fc0b63
Subhadeep Paul <subhapaul637@gmail.com>
2026-03-17 16:41:19 +0530

    Time: 11 ms (69.18%), Space: 60 MB (56.43%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    const map = new Map();
+    for(let n of nums){
+        map.set(n,(map.get(n)|| 0)+1);
+    }
+    return[...map.entries()]
+    .sort((a,b)=>b[1]-a[1]).slice(0,k).map(entry=>entry[0]);
+};
