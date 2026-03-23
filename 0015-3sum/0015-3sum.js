e9e068dc307bd7355e08dd192e23acf866ab80e0
Subhadeep Paul <subhapaul637@gmail.com>
2026-03-23 21:55:49 +0530

    Time: 35 ms (66.82%), Space: 73.4 MB (16.81%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,8 +2,8 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function (nums) {
-    const res = [];
+var threeSum = function(nums) {
+     const res = [];
     nums.sort((a, b) => a - b);
     for (let i = 0; i < nums.length - 2; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) {
