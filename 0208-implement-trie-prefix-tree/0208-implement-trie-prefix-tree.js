94690e00589e67a6110f08cbc6937c4402d5def3
Subhadeep Paul <subhapaul637@gmail.com>
2026-04-24 23:01:38 +0530

    Time: 54 ms (21.65%), Space: 72.2 MB (87.78%) - LeetHub

---
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -0,0 +1,30 @@
+var Trie = function() {
+    this.root = {};
+};
+
+Trie.prototype.insert = function(word) {
+    let node = this.root;
+    for (let char of word) {
+        if (!node[char]) node[char] = {};
+        node = node[char];
+    }
+    node.isEnd = true;
+};
+
+Trie.prototype.search = function(word) {
+    let node = this.root;
+    for (let char of word) {
+        if (!node[char]) return false;
+        node = node[char];
+    }
+    return node.isEnd === true;
+};
+
+Trie.prototype.startsWith = function(prefix) {
+    let node = this.root;
+    for (let char of prefix) {
+        if (!node[char]) return false;
+        node = node[char];
+    }
+    return true;
+};
