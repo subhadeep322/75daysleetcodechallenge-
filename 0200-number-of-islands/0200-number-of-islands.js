25f7b9abec5bb504d4afe44b2ab08f6ceaad0f3a
Subhadeep Paul <subhapaul637@gmail.com>
2026-05-28 20:45:07 +0530

    Time: 59 ms (71.84%), Space: 58.9 MB (60.12%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.js
@@ -0,0 +1,37 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    if (!grid || grid.length === 0) {
+        return 0;
+    }
+    
+    let islandCount = 0;
+    const rows = grid.length;
+    const cols = grid[0].length;
+    
+        const dfs = (r, c) => {
+        if (r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] === '0') {
+            return;
+        }
+        
+        grid[r][c] = '0';
+        
+        dfs(r - 1, c); // up
+        dfs(r + 1, c); // down
+        dfs(r, c - 1); // left
+        dfs(r, c + 1); // right
+    };
+    
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < cols; c++) {
+            if (grid[r][c] === '1') {
+                islandCount++; 
+                dfs(r, c);   
+            }
+        }
+    }
+    
+    return islandCount;
+};
